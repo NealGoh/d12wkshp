c826bc3e8b52443d308a9aea1da07c6f6fe0aa31
Neal Goh <nealgoh1996@gmail.com>
2022-07-19 15:03:38 +0800

    workshop

--- src/main/java/sg/edu/nus/iss/day12wkshp/WorkshopController.java
+++ src/main/java/sg/edu/nus/iss/day12wkshp/WorkshopController.java
@@ -1,13 +1,22 @@
 package sg.edu.nus.iss.day12wkshp;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WorkshopController {
     
     @RequestMapping(path={ "/workshop"})
     public String test() {
+        return "workshop";
+    }
+
+    public String testResults(
+        @RequestParam(name = "inputNumber", defaultValue = "1") Integer unit,
+        Model model) {
+
         return "workshop";
     }
 }
