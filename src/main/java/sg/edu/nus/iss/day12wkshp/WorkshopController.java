0304d5ea444a1f9209744d1c2b47aa29740342e4
Neal Goh <nealgoh1996@gmail.com>
2022-07-19 16:26:46 +0800

    workshop 2 completed

--- src/main/java/sg/edu/nus/iss/day12wkshp/WorkshopController.java
+++ src/main/java/sg/edu/nus/iss/day12wkshp/WorkshopController.java
@@ -1,5 +1,7 @@
 package sg.edu.nus.iss.day12wkshp;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,15 +10,37 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class WorkshopController {
     
-    @RequestMapping(path={ "/workshop"})
-    public String test() {
-        return "workshop";
-    }
+        @RequestMapping(path={ "/workshop"})
+        public String test() {
+            return "workshop";
+        }
 
-    public String testResults(
-        @RequestParam(name = "inputNumber", defaultValue = "1") Integer unit,
-        Model model) {
+        @RequestMapping(path = {"/processNumber"})
+        public String testResults(
+            @RequestParam(name = "inputNumber", defaultValue = "1") Integer unit,
+            Model model) {
+        
+        List<Integer> lstInt = new ArrayList<>();
+        int loopValue = Integer.parseInt(unit.toString());
 
-        return "workshop";
+        int loop = 1;
+        while (loop <= loopValue) {
+
+            int result = (int) ((Math.random() * loopValue) + 1);
+
+            if (!lstInt.contains(Integer.valueOf(result))) {
+                lstInt.add(Integer.valueOf(result));
+                loop++;
+            }
+        }
+
+        for (int i = 0; i < lstInt.size(); i++) {
+            System.out.print("==> " + lstInt.get(i));
+            System.out.println();
+        }
+            
+            model.addAttribute("numbers", lstInt);
+
+            return "workshopresult";
     }
 }
